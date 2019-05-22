aa11af2a46e66fcc190406c1a6181d2d0bfa83aa
Ibrahima sory TRAORE <ibrahima982011@hotmail.fr>
2019-05-22 17:30:21 +0000

    Création du projet spring boot

---
+++ src/main/java/gn/traore/DemoSpringBootKeycloakApplication.java
@@ -0,0 +1,13 @@
+package gn.traore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoSpringBootKeycloakApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoSpringBootKeycloakApplication.class, args);
+	}
+
+}
